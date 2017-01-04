29280b9eca48dd69ebdf61d3b879a89b097a8da1
Zhang <zhn_howl@qq.com>
2017-01-04 10:15:16 +0800

    初始化框架

---
+++ RealCostReport/Infrastucture.TreeModels/TreeModels/TreeModels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.TreeModels.TreeModels
+{
+    public class LeafBase
+    {
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public int Level { set; get; }
+
+        public virtual void CopyNodeValue(LeafBase leaf)
+        {
+            this.Id = leaf.Id;
+            this.Level = leaf.Level;
+            this.Name = leaf.Name;
+        }
+    }
+
+    public class TreeBase<T> where T : new()
+    {
+        public int Id { set; get; }
+
+        public T Node { set; get; }
+
+        public TreeStauts Status { set; get; }
+        public TreeBase<T> Parent { set; get; }
+        public virtual List<TreeBase<T>> Child { set; get; }
+    }
+
+    public class LabelTrees
+    {
+        public int id { set; get; }
+
+        public String Labels { set; get; }
+        public TreeBase<LeafBase> LabelTree { set; get; }
+    }
+
+
+    public enum TreeStauts
+    {
+        Unchanged,
+        NewNode,
+        Delete,
+        Modified
+    }
+}
---
+++ RealCostReport/Infrastucture.TreeModels/TreeServices/ITreeServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.TreeModels
+{
+    public interface ITreeServices<T> where T : class
+    {
+        IQueryable<T> QueryBy(Expression<Func<T, bool>> predict);
+        IQueryable<T> QueryByWithDescendants(Expression<Func<T, List<T>>> include, Expression<Func<T, bool>> predict);
+        void SaveChanges();
+    }
+}
